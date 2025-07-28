604487646af475fa43238f9fa904b71b2c99cfeb
kang <hgk240318@gmail.com>
2025-07-28 17:21:57 +0900

    edit-25.07.28

--- app/routers/upload_proxy.py
+++ app/routers/upload_proxy.py
@@ -10,9 +10,7 @@ router = APIRouter()
     description="이 API는 Dropzone 업로더를 통해 자동 호출됩니다.\n직접 사용하지 마시고 [http://localhost:8000/uploader](http://localhost:8000/uploader)에서 업로드하세요."
 )
 async def upload_direct(
-    file: UploadFile = File(...),
-    bucket_name: str = Query(...)
+    file: UploadFile = File(...)
 ):
-    ensure_bucket(bucket_name)
-    upload_file(file.file, file.filename, bucket_name)
-    return {"msg": f"{file.filename} uploaded to {bucket_name}"}
+    upload_file(file.file, file.filename)
+    return {"filename": file.filename}
