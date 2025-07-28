8c291deaafcc4f9ddda221704df42dc96b670322
kang <hgk240318@gmail.com>
2025-07-28 11:33:45 +0900

    if __name__ == "__main__": 주석처리

--- app/main.py
+++ app/main.py
@@ -12,5 +12,5 @@ app.mount("/uploader", StaticFiles(directory="static/uploader", html=True), name
 def init():
     ensure_bucket("yolo-train") # 지금은 버킷이름을 이렇게 지정해 주어야 함.
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
